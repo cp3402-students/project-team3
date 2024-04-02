24c0d02091dcd2d818af154de2d21cea39939068
John Short <cytlan@shibesoft.com>
2024-04-02 23:07:31 +0200

    Add footer widgets and style

--- footer.php
+++ footer.php
@@ -12,18 +12,13 @@
 ?>
 
 	<footer id="colophon" class="site-footer">
+		<div class="footer-columns">
+			<?php dynamic_sidebar('footer_area_one'); ?>
+			<?php dynamic_sidebar('footer_area_two'); ?>
+			<?php dynamic_sidebar('footer_area_three'); ?>
+		</div>
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'u3a-theme' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'u3a-theme' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'u3a-theme' ), 'u3a-theme', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
+			Copyright © 2024, All Rights Reserved
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
--- inc/template-functions.php
+++ inc/template-functions.php
@@ -35,3 +35,40 @@ function u3a_theme_pingback_header() {
 	}
 }
 add_action( 'wp_head', 'u3a_theme_pingback_header' );
+
+/**
+ * Add widget areas to the footer
+ */
+function u3a_theme_footer_widgets()
+{
+	register_sidebar([
+		'name'          => 'Footer area one',
+		'id'            => 'footer_area_one',
+		'description'   => 'This widget area discription',
+		'before_widget' => '<section class="footer-area footer-area-one">',
+		'after_widget'  => '</section>',
+		'before_title'  => '<h4>',
+		'after_title'   => '</h4>',
+	]);
+
+	register_sidebar([
+		'name'          => 'Footer area two',
+		'id'            => 'footer_area_two',
+		'description'   => 'This widget area discription',
+		'before_widget' => '<section class="footer-area footer-area-two">',
+		'after_widget'  => '</section>',
+		'before_title'  => '<h4>',
+		'after_title'   => '</h4>',
+	]);
+
+	register_sidebar([
+		'name'          => 'Footer area three',
+		'id'            => 'footer_area_three',
+		'description'   => 'This widget area discription',
+		'before_widget' => '<section class="footer-area footer-area-three">',
+		'after_widget'  => '</section>',
+		'before_title'  => '<h4>',
+		'after_title'   => '</h4>',
+	]);
+}
+add_action('widgets_init', 'u3a_theme_footer_widgets');
