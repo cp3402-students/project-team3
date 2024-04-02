46817ca51fc2caf9e5a520c4dfa6d1a074a289a4
John Short <cytlan@shibesoft.com>
2024-04-03 00:46:03 +0200

    Add header info option to customizer

--- inc/customizer.php
+++ inc/customizer.php
@@ -11,6 +11,34 @@
  * @param WP_Customize_Manager $wp_customize Theme Customizer object.
  */
 function u3a_theme_customize_register( $wp_customize ) {
+	$wp_customize->add_section( 'u3a_theme_site_text', [
+		'title' => __('Site Text'),
+		'description' => __( 'Change site text here' ),
+		'priority' => 160,
+		'capability' => 'edit_theme_options',
+	]);
+
+	// Header info text
+	$wp_customize->add_setting('u3a_theme_info_text', [
+		'default'           => __( '(07)466 777 777 | admin@u3aonline.org.au'),
+		'sanitize_callback' => 'sanitize_text_field',
+		'transport'         => 'postMessage',
+	]);
+	$wp_customize->add_control('u3a_theme_info_text', [
+		'type'        => 'text',
+		'priority'    => 10,
+		'section'     => 'u3a_theme_site_text',
+		'label'       => __('Header Info'),
+		'description' => __('Text that appears on the top right-hand corner')
+	]);
+	$wp_customize->selective_refresh->add_partial('u3a_theme_info_text', [
+		'selector'            => '.info-text',
+		'container_inclusive' => false,
+		'render_callback'     => function() {
+			echo get_theme_mod('u3a_theme_info_text');
+		},
+	]);
+
 	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
 	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
 	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
